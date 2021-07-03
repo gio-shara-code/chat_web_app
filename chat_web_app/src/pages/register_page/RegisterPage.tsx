f9c03e3dcee79b2327f562a27824aa6fde7bd817
Giorgi Sharashenidze <giorgi.sharashenidze@code.berlin>
2021-07-03 22:59:30 +0200

    Register Page UI Done

--- chat_web_app/src/pages/register_page/RegisterPage.tsx
+++ chat_web_app/src/pages/register_page/RegisterPage.tsx
@@ -1,14 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+import PrimaryInput from '../../components/inputs/PrimaryInput'
 
 interface Props {
     setToken: (token: string) => void
 }
 const RegisterPage: React.FC<Props> = () => {
-    return <div>
+    const [email, setEmail] = useState<string>('gio.sharashenidze@code.berlin')
+    const [fullName, setFullName] = useState<string>('Giorgi Sharashenidze')
+    const [password, setPassword] = useState<string>('123456')
 
-        
+    const onEmailChange = (e: any) => setEmail(e.target.value)
 
-    </div>
+    const onPasswordChange = (e: any) => setPassword(e.target.value)
+    const onFullNameChange = (e: any) => setFullName(e.target.value)
+    const onLoginInButtonClick = () => {
+        //Make login request and get token
+    }
+    return (
+        <div className="flex flex-col px-3 pt-24">
+            <label>Name:</label>
+            <PrimaryInput type="text" value={fullName} onChange={onFullNameChange} />
+
+            <br />
+            <label>Email:</label>
+            <PrimaryInput type="email" value={email} onChange={onEmailChange} />
+
+            <br />
+            <label>Password:</label>
+            <PrimaryInput type="password" value={password} onChange={onPasswordChange} />
+
+            <br />
+            <div className="text-center">
+                <button className="border-2 border-black px-2" onClick={onLoginInButtonClick}>
+                    Register
+                </button>
+            </div>
+        </div>
+    )
 }
 
 export default RegisterPage
