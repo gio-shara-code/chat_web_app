17b9c6921c4a2d67232919b1fe74df9fa0a680c3
Giorgi Sharashenidze <giorgi.sharashenidze@code.berlin>
2021-07-03 22:59:14 +0200

    Removed full name field because its unneccessary

--- chat_web_app/src/pages/login_page/LoginPage.tsx
+++ chat_web_app/src/pages/login_page/LoginPage.tsx
@@ -2,21 +2,18 @@ import React, { useState } from 'react'
 import PrimaryInput from '../../components/inputs/PrimaryInput'
 const LoginPage = () => {
     const [email, setEmail] = useState<string>('gio.sharashenidze@code.berlin')
-    const [fullName, setFullName] = useState<string>('Giorgi Sharashenidze')
+
     const [password, setPassword] = useState<string>('123456')
 
     const onEmailChange = (e: any) => setEmail(e.target.value)
 
     const onPasswordChange = (e: any) => setPassword(e.target.value)
-    const onFullNameChange = (e: any) => setFullName(e.target.value)
+
     const onLoginInButtonClick = () => {
         //Make login request and get token
     }
     return (
         <div className="flex flex-col px-3 pt-24">
-            <label>Name:</label>
-            <PrimaryInput type="text" value={fullName} onChange={onFullNameChange} />
-
             <br />
             <label>Email:</label>
             <PrimaryInput type="email" value={email} onChange={onEmailChange} />
@@ -26,7 +23,7 @@ const LoginPage = () => {
             <PrimaryInput type="password" value={password} onChange={onPasswordChange} />
 
             <br />
-            <div className='text-center'>
+            <div className="text-center">
                 <button className="border-2 border-black px-2" onClick={onLoginInButtonClick}>
                     Login
                 </button>
