d6fc0a87d6f7a62768d93474ab16de208f49ac53
Giorgi Sharashenidze <giorgi.sharashenidze@code.berlin>
2021-07-04 12:49:00 +0200

    Added config for tailwind and autoprefixer

--- chat_web_app/postcss.config.js
+++ chat_web_app/postcss.config.js
@@ -1,7 +1,7 @@
-// postcss.config.js
+// // postcss.config.js
 module.exports = {
     plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    }
-  }
+        tailwindcss: {},
+        autoprefixer: {},
+    },
+}
