c5262cf56281d9e6c176e7339e76bea30a40676f
Giorgi Sharashenidze <giorgi.sharashenidze@code.berlin>
2021-07-04 12:06:10 +0200

    Imported axios and Added config

--- chat_web_app/src/index.tsx
+++ chat_web_app/src/index.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "tailwindcss/tailwind.css";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import 'tailwindcss/tailwind.css'
 import './index.css'
-// import axios from "axios";
+import axios from 'axios'
 
-// axios.defaults.baseURL = "http://localhost:8000";
+axios.defaults.baseURL = 'http://localhost:8000'
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'))
