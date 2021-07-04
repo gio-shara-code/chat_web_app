c2e81c98372d47c26e593cc128277838425897f3
Giorgi Sharashenidze <giorgi.sharashenidze@code.berlin>
2021-07-04 15:57:50 +0200

    Added user routes and morgan

--- server/src/loaders/express_loader.ts
+++ server/src/loaders/express_loader.ts
@@ -2,13 +2,17 @@ import {Application} from "express"
 import {json, urlencoded} from "body-parser"
 import cors from "cors"
 import authRoutes from "../routes/auth_routes"
+import userRoutes from "../routes/user_routes"
+import morgan from "morgan"
 
 const expressLoader = async (app: Application) => {
   app.use(json())
   app.use(urlencoded({extended: false}))
   app.use(cors())
+  app.use(morgan(":method :url :status :res[content-length] - :response-time ms"))
 
   app.use("/auth", authRoutes)
+  app.use(userRoutes)
 
   return app
 }
