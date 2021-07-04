f6e8803ab6f52bd98731706d23a780055bdf0bfb
Giorgi Sharashenidze <giorgi.sharashenidze@code.berlin>
2021-07-04 15:33:27 +0200

    Search tab ui done

--- chat_web_app/src/pages/home_page/side_bar/side_bar_tabs/search_tab/SearchTab.tsx
+++ chat_web_app/src/pages/home_page/side_bar/side_bar_tabs/search_tab/SearchTab.tsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { User } from '../../../../../interfaces/user'
+import SearchContactList from './SearchContactList'
 
 const searchTab = () => {
-    return <div className="flex-1 flex items-center justify-center px-3 bg-hell-blue rounded-t-3xl">Search</div>
+    const [isLoading, setLoading] = useState<boolean>(false)
+
+    const [searchedContactName, setSearchContactName] = useState<string>('')
+
+    const [foundContacts, setFoundContacts] = useState<User[]>([])
+
+    const toggleLoadingProcess = () => setLoading((isLoading) => !isLoading)
+
+    const onSearchContactInputChange = (e: any) => {
+        setSearchContactName(e.target.value)
+    }
+
+    const onSearchContactInputKeyDown = (e: any) => {
+        if (e.code === 'Enter') {
+            //Search for contacts
+
+            toggleLoadingProcess()
+
+            setTimeout(() => {
+                toggleLoadingProcess()
+            }, 1000)
+        }
+    }
+
+    return (
+        <div className="flex-1 flex flex-col justify-start px-3 bg-hell-blue rounded-t-3xl">
+            <div className="py-3">
+                <input onKeyDown={onSearchContactInputKeyDown} className="w-full py-2 px-3" type="text" value={searchedContactName} onChange={onSearchContactInputChange} placeholder="Search for a new contact..." />
+            </div>
+
+            <SearchContactList searchingForContacts={isLoading} foundContacts={foundContacts} searchedContactName={searchedContactName} />
+        </div>
+    )
 }
 
 export default searchTab
