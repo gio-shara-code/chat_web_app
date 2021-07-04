fb579f34473b9cfe2dd630be7853c22f724c53c6
Giorgi Sharashenidze <giorgi.sharashenidze@code.berlin>
2021-07-04 02:10:04 +0200

    token handlig done

---
+++ server/src/services/jwt.ts
@@ -0,0 +1,32 @@
+import jwt from "jsonwebtoken"
+import config from "../config"
+
+interface TokenData {
+  _id: string
+  email: string
+}
+
+const signToken = async (data: TokenData) => {
+  let result
+  try {
+    result = await jwt.sign({id: data._id, email: data.email}, config.jWTSecretKey, {
+      algorithm: "HS256"
+    })
+  } catch (e) {
+    console.log(`Signing token failed. ${e}`)
+    return
+  }
+  return result
+}
+
+const compareToken = async (token: string) => {
+  let result
+  try {
+    result = await jwt.verify(token, config.jWTSecretKey)
+  } catch (e) {
+    console.log(`Signing token failed. ${e}`)
+    return
+  }
+  return result
+}
+export {signToken, compareToken}
