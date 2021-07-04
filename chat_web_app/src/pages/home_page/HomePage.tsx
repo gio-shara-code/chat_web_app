5c30e43348ecc3e66f803c3ed7b625a946870dbb
Giorgi Sharashenidze <giorgi.sharashenidze@code.berlin>
2021-07-04 15:59:48 +0200

    Added user object as state

--- chat_web_app/src/pages/home_page/HomePage.tsx
+++ chat_web_app/src/pages/home_page/HomePage.tsx
@@ -2,12 +2,14 @@ import React, { useEffect, useState } from 'react'
 import SideBar from './side_bar/SideBar'
 import ChatArea from './chat_area/ChatArea'
 import { SelectedMessageIdContext } from '../../context/selected_contact_id_context'
+import { User } from '../../interfaces/user'
 
 const HomePage = () => {
     const [selectedContactId, setSelectedContactId] = useState<string>('')
+    const [user, setUser] = useState<User>()
 
     const componentDidMount = () => {
-        //Connect to the server
+        //Connect to the server 
     }
     useEffect(componentDidMount, [])
 
