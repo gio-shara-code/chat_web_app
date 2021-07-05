3aef1a868d5b68b39e9816b8c4cc13f7ba895c66
Giorgi Sharashenidze <giorgi.sharashenidze@code.berlin>
2021-07-05 16:01:02 +0200

    Deleted unnecessary headers

--- chat_web_app/src/api/user_api.ts
+++ chat_web_app/src/api/user_api.ts
@@ -3,7 +3,6 @@ import { ApiResponse } from '../interfaces/api_response'
 
 const getUser = async (token: string): Promise<ApiResponse> => {
     const headers = {
-        'Content-Type': 'Application/json',
         authorization: `Bearer ${token}`,
     }
 
@@ -26,4 +25,28 @@ const getUser = async (token: string): Promise<ApiResponse> => {
     }
 }
 
-export { getUser }
+const getUsersByName = async (token: string, name: string): Promise<ApiResponse> => {
+    const headers = {
+        authorization: `Bearer ${token}`,
+    }
+
+    let res: AxiosResponse
+    try {
+        res = await axios.get(`/users?name=${name}`, {
+            headers: headers,
+        })
+    } catch (e) {
+        console.log(`user_api.ts[getUser]: ${e}`)
+        return {
+            success: false,
+            message: 'Fetching users by name failed.',
+        }
+    }
+
+    return {
+        success: true,
+        users: res.data.users,
+    }
+}
+
+export { getUser, getUsersByName }
