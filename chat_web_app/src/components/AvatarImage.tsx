32a798d991f91c0acc77e6cfdb103ae06fe9f425
Giorgi Sharashenidze <giorgi.sharashenidze@code.berlin>
2021-07-05 17:58:46 +0200

    Added new props to AvatarImage

--- chat_web_app/src/components/AvatarImage.tsx
+++ chat_web_app/src/components/AvatarImage.tsx
@@ -5,13 +5,15 @@ interface Props {
     src: string
     alt: string
     status?: Status | undefined
+    width: string
+    height: string
 }
 
-const AvatarImage: React.FC<Props> = ({ src, alt, status }) => {
+const AvatarImage: React.FC<Props> = ({ src, alt, status, width, height }) => {
     let statusColor: string = status ? getColorBasedOnStatus(status) : ''
-
+    //w-55 h-55
     return (
-        <div className="w-55 h-55 inline-block align-middle relative">
+        <div className={`${width} ${height} inline-block align-middle relative`}>
             <div className={`rounded-full h-3 w-3 absolute right-0.5 top-0.5  ${status && `border-white border-1.5 bg-${statusColor}-500`}`}></div>
             <img className="w-full h-full rounded-full object-cover border-yellow border-2" src={src} alt={alt} />
         </div>
