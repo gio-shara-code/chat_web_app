db24406e099bda6a71ca36cba249e4a46229a5de
Giorgi Sharashenidze <giorgi.sharashenidze@code.berlin>
2021-07-05 17:53:03 +0200

    I was going to create contact model

---
+++ server/src/models/contact_model.ts
@@ -0,0 +1,14 @@
+// import {model, Schema, Document} from "mongoose"
+// import {Contact} from "../interfaces/contact"
+// //When creating a document
+
+// const userSchema = new Schema({
+//     name: string
+//     lastMessageSeen: boolean
+//     lastMessage: string
+//     lastMessageDateNumber: number
+//     lastMessageFromId: string
+//     status: Status
+// })
+
+// export default model<Contact & Document>("User", userSchema)
