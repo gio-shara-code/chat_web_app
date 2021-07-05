08666832380d5863ea6e9fba439c233e4c632f5e
Giorgi Sharashenidze <giorgi.sharashenidze@code.berlin>
2021-07-05 12:43:41 +0200

    Fetching user when user logs in  and set up user context for passing the state down the component tree easily

--- chat_web_app/src/pages/home_page/HomePage.tsx
+++ chat_web_app/src/pages/home_page/HomePage.tsx
@@ -1,25 +1,56 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import SideBar from './side_bar/SideBar'
 import ChatArea from './chat_area/ChatArea'
 import { SelectedMessageIdContext } from '../../context/selected_contact_id_context'
 import { User } from '../../interfaces/user'
-
+import { getUser } from '../../api/user_api'
+import { TokenContext } from '../../context/token_context'
+import { UserContext } from '../../context/user_context'
 const HomePage = () => {
     const [selectedContactId, setSelectedContactId] = useState<string>('')
-    const [user, setUser] = useState<User>()
+    const [user, setUser] = useState<User | undefined>()
+    const token = useContext(TokenContext)
+
+    const [isLoading, setIsLoading] = useState<boolean>(false)
+
+    const toggleLoadingProcess = () => setIsLoading((isLoading) => !isLoading)
+
+    const fetchUserInformation = async () => {
+        toggleLoadingProcess()
+        setTimeout(async () => {
+            const res = await getUser(token.value as string)
+            if (res.success) {
+                setUser(res.user)
+            } else {
+                console.log(res.message)
+            }
+            toggleLoadingProcess()
+        }, 2000)
+    }
 
     const componentDidMount = () => {
-        //Connect to the server 
+        fetchUserInformation()
     }
     useEffect(componentDidMount, [])
 
+    if (isLoading) {
+        return <div className="flex justify-center items-center h-full text-3xl">Welcome to Chat Web App...</div>
+    }
+
     return (
-        <div className="flex align-center h-full">
+        <UserContext.Provider
+            value={{
+                set: setUser,
+                value: user,
+            }}
+        >
             <SelectedMessageIdContext.Provider value={{ value: selectedContactId, set: setSelectedContactId }}>
-                <SideBar />
-                <ChatArea />
+                <div className="flex h-full">
+                    <SideBar />
+                    <ChatArea />
+                </div>
             </SelectedMessageIdContext.Provider>
-        </div>
+        </UserContext.Provider>
     )
 }
 
