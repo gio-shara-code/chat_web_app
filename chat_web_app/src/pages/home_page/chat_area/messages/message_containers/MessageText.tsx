50914623dcb88a3814943bd9fafbd2611ca0f903
Giorgi Sharashenidze <giorgi.sharashenidze@code.berlin>
2021-07-05 17:55:34 +0200

    Created MessageText component

---
+++ chat_web_app/src/pages/home_page/chat_area/messages/message_containers/MessageText.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Message } from '../../../../../interfaces/message'
+import SeenMessageIcon from '../../../../../assets/icons/seen_message.svg'
+import { formatDateNumberToHourMinute } from '../../../../../utils/format_date_utils'
+interface Props {
+    message: Message
+    itsMe: boolean
+    onProfileClick(): void
+}
+
+const MessageText: React.FC<Props> = ({ message, itsMe, onProfileClick }) => {
+    return (
+        <div className={`flex flex-col items-${itsMe ? 'end' : 'start'} p-3`}>
+            <div className='cursor-pointer' onClick={onProfileClick}>{message.author}</div>
+
+            <div className={`flex flex-col items-${itsMe ? 'end' : 'start'} bg-${itsMe ? 'red-200' : 'hell-blue'} p-5 max-w-screen-sm rounded-2xl rounded-t${itsMe ? 'r' : 'l'}-none`}>
+                {message.content}
+                <div className={`flex justify-end items-center w-full`}>
+                    <span className={`${itsMe ? 'pr-3' : ''}`}>{formatDateNumberToHourMinute(message.sentOn)}</span>
+                    {itsMe && <SeenMessageIcon />}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MessageText
