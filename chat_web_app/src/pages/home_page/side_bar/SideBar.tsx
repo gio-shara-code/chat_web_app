72650849ae9a641dc946fcef712aac6618a307c7
Giorgi Sharashenidze <giorgi.sharashenidze@code.berlin>
2021-07-05 15:58:09 +0200

    Renamed enum value

--- chat_web_app/src/pages/home_page/side_bar/SideBar.tsx
+++ chat_web_app/src/pages/home_page/side_bar/SideBar.tsx
@@ -3,18 +3,18 @@ import { Tab } from '../../../enums/tab'
 import ProfileContainer from '../ProfileContainer'
 import BottomNavigationButtons from './bottom_navigation_buttons/BottomNavigationButtons'
 import ContactsTab from './side_bar_tabs/contacts_tab/ContactsTab'
-import ContactTab from './side_bar_tabs/messaged_contacts_tab/MessagedContactsTab'
+import MessagedContactsTab from './side_bar_tabs/messaged_contacts_tab/MessagedContactsTab'
 import FavoriteTab from './side_bar_tabs/favorite_tab/FavoriteTab'
 import SearchTab from './side_bar_tabs/search_tab/SearchTab'
 
 const SideBar = () => {
-    const [currentTab, setCurrentTab] = useState<Tab>(Tab.contact)
+    const [currentTab, setCurrentTab] = useState<Tab>(Tab.messageContactTab)
     const onNavBarButtonClick = (tab: Tab) => setCurrentTab(tab)
-    
-    return ( 
+
+    return (
         <aside className="flex flex-col w-1/6 min-w-450">
             <ProfileContainer />
-            {Tab.contact === currentTab && <ContactTab />}
+            {Tab.messageContactTab === currentTab && <MessagedContactsTab />}
             {Tab.contacts === currentTab && <ContactsTab />}
             {Tab.search === currentTab && <SearchTab />}
             {Tab.favorite === currentTab && <FavoriteTab />}
