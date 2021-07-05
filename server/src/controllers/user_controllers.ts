9507d8a75c1de785e834a88c765cd83a3e9d0a70
Giorgi Sharashenidze <giorgi.sharashenidze@code.berlin>
2021-07-05 12:40:06 +0200

    Adding email query param logic

--- server/src/controllers/user_controllers.ts
+++ server/src/controllers/user_controllers.ts
@@ -1,13 +1,16 @@
-import {Request, Response} from "express"
+import {Request, Response, NextFunction} from "express"
 import {ResponseBody} from "../interfaces/response_body"
 import {User} from "../interfaces/user"
-import {getUserById} from "../services/db/user_services"
+import * as userServices from "../services/db/user_services"
 
-const getUser = async (req: Request, res: Response<ResponseBody>) => {
+const getUser = async (req: Request, res: Response<ResponseBody>, next: NextFunction) => {
+  const {email} = req.query
+
+  if (email) return next()
+
   let user
-
   try {
-    user = await getUserById(req.body.userId)
+    user = await userServices.getUserById(req.body.userId)
     if (!user) throw new Error("User not found.")
   } catch (e: any) {
     return res.json({
@@ -22,4 +25,25 @@ const getUser = async (req: Request, res: Response<ResponseBody>) => {
   })
 }
 
-export {getUser}
+const getUserByEmail = async (req: Request, res: Response<ResponseBody>) => {
+  const {email} = req.query
+
+  let user: User
+
+  try {
+    user = (await userServices.getUserByEmail(email as string)) as User
+    if (!user) throw new Error("Contact not found")
+  } catch (e: any) {
+    return res.json({
+      success: false,
+      message: e.message
+    })
+  }
+
+  res.status(200).json({
+    success: true,
+    user: user
+  })
+}
+
+export {getUser, getUserByEmail}
