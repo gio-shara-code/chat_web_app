c2b353a4580eab7bae71ce2f36d4726916d838de
Giorgi Sharashenidze <giorgi.sharashenidze@code.berlin>
2021-07-05 17:57:23 +0200

    Added ProfileSidebar logic

--- chat_web_app/src/pages/home_page/chat_area/ChatArea.tsx
+++ chat_web_app/src/pages/home_page/chat_area/ChatArea.tsx
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ChatController from './ChatController'
-import MessagesArea from './MessagesArea'
+import Messages from './messages/Messages'
+import ProfileSidebar from './messages/profile_sidebar/ProfileSidebar'
 
 const ChatArea = () => {
+    const [isProfileSidebarVisible, setProfileSidebarVisiblity] = useState<boolean>(true)
+
+    const toggleProfileSidebarVisibility = () => setProfileSidebarVisiblity((visibility) => !visibility)
+
+    const onProfileClick = () => {
+        toggleProfileSidebarVisibility()
+    }
+
+    const onCloseSidebarButtonClick = () => toggleProfileSidebarVisibility()
     return (
-        <main className="flex-1 flex flex-col ">
+        <main className="flex-1 flex">
             {/* Chat | Media */}
-            <MessagesArea />
-            <ChatController />
+            <div className="flex-1 flex flex-col">
+                <Messages onProfileClick={onProfileClick} />
+                <ChatController />
+            </div>
+
+            {isProfileSidebarVisible && <ProfileSidebar onClose={onCloseSidebarButtonClick} />}
         </main>
     )
 }
 
 export default ChatArea
-    
